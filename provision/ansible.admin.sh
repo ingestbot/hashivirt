22d7b06a75288df2a4a8a2099cca3a912480a852
ingestbot <ingestbot>
2024-06-05 18:22:56 -0700

    ansible.admin.sh now uses /etc/ssh/authorized_keys/ansible.admin

--- provision/ansible.admin.sh
+++ provision/ansible.admin.sh
@@ -8,14 +8,13 @@ groupadd -g 7000 ansible.admin
 useradd -g 7000 -u 7000 ansible.admin
 usermod -a -G sudo ansible.admin
 
-mkdir -p /home/ansible.admin/.ssh
-
-curl -k -so /home/ansible.admin/.ssh/authorized_keys https://${HTTPSERVER}/admin/ssh/ansible.admin.ssh.pub.key 
-
-chown -R ansible.admin /home/ansible.admin
-chgrp -R ansible.admin /home/ansible.admin
-chmod 700 /home/ansible.admin/.ssh
-chmod 400 /home/ansible.admin/.ssh/authorized_keys
+mkdir /etc/ssh/authorized_keys
+chmod 755 /etc/ssh/authorized_keys
+curl -k -so /etc/ssh/authorized_keys/ansible.admin https://${HTTPSERVER}/admin/ssh/ansible.admin.ssh.pub.key
+chown ansible.admin /etc/ssh/authorized_keys/ansible.admin
+chgrp ansible.admin /etc/ssh/authorized_keys/ansible.admin
+chmod 400 /etc/ssh/authorized_keys/ansible.admin
+echo 'AuthorizedKeysFile /etc/ssh/authorized_keys/%u' > /etc/ssh/sshd_config.d/00-provision_tmp.conf
 
 echo 'ansible.admin ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/99_ansible_admin
 chmod 640 /etc/sudoers.d/99_ansible_admin
